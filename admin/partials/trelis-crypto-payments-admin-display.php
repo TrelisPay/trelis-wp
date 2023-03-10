2a47f4799777aaaf47cf2044cd64c36a394ba3ee
Jaldev09 <jalpesh.fullstack10@gmail.com>
2023-03-10 17:04:32 +0530

    code merged

---
+++ admin/partials/trelis-crypto-payments-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://www.Trelis.com
+ * @since      1.0.0
+ *
+ * @package    Trelis_Crypto_Payments
+ * @subpackage Trelis_Crypto_Payments/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ public/partials/trelis-crypto-payments-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://www.Trelis.com
+ * @since      1.0.0
+ *
+ * @package    Trelis_Crypto_Payments
+ * @subpackage Trelis_Crypto_Payments/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
